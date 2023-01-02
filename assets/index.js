e1391cb920024846a8ec82c88de6777045407ff4
edm1001 <edmer_franciz@yahoo.com>
2023-01-02 12:17:27 -0600

    add save tab function

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,38 @@
 
-
 let myLeads = [] 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem(myLeads))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage // set myLeads to its value
-    renderLeads()
+    render(myLeads)
 } 
 
-function renderLeads() {   
+const tabs = [
+    {url:"www.linkedin.com/in/edmer-valencia-080414227"}
+]
+
+tabBtn.addEventListener("click", function(){   
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {          
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("url", JSON.stringify(myLeads)) 
+        render(myLeads);
+    })
+})
+
+
+function render(leads) {   
     let listItems = " "
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-        <a target='_blank' href='${myLeads[i]}'>
-        ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
         </a>
         </li>`
         // `` allows html to be written cleaner with less ''""
@@ -30,7 +44,7 @@ function renderLeads() {
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     myLeads = [] //set it to an empty array
-    renderLeads()
+    render(myLeads)
 })
 
 
@@ -39,7 +53,7 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = "" //resets the input to blank after saving
     localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-    renderLeads();
+    render(myLeads);
     console.log(localStorage.getItem("myLeads"))
 })
 
@@ -55,3 +69,9 @@ inputBtn.addEventListener("click", function() {
 //     buy.innerHTML += '<p>Thank You For Purchasing</p>'
 // } //renders the p tag when u click buy-btn
 
+// function getFirst(arr) {
+//     return arr[0]
+// }
+// let firstCard = getFirst([0,10, 8]) 
+// console.log(firstCard)
+//uses a function getFirst() to get the first item in the array of firstCard
